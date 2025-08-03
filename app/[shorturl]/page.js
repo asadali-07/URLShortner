f20c7d11cfa35d6a5fe322b8a86aa8e1ab8a9797
Asad Ali <mo.asadali007@gmail.com>
2025-08-04 01:53:50 +0530

    Make responsive and Deploy on Netlify

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -8,15 +8,8 @@ export default async function Page({ params }) {
     const client = await clientPromise;
     const db = client.db("bitlinks")
     const collection = db.collection("url")
-
     const doc = await collection.findOne({shorturl: shorturl})
-    console.log(doc)
-    if(doc){
-         redirect(doc.url)
-    }
-    else{
-        redirect(`${process.env.NEXT_PUBLIC_HOST}`)
-    }
 
-    return <div>My Post: {url}</div>
+    if(doc) redirect(doc.url)
+    redirect(`${process.env.NEXT_PUBLIC_HOST}`)
   }
--- app/shorten/page.js
+++ app/shorten/page.js
@@ -8,8 +8,10 @@ const Shorten = () => {
     const [url, seturl] = useState("")
     const [shorturl, setshorturl] = useState("")
     const [generated, setGenerated] = useState("")
+    const[loading, setLoading] = useState(false)
 
     const generate = () => {
+        setLoading(true)
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
 
@@ -31,7 +33,7 @@ const Shorten = () => {
                 setGenerated(`${process.env.NEXT_PUBLIC_HOST}/${shorturl}`)
                 seturl("")   
                 setshorturl("")
-                console.log(result)
+                setLoading(false)
                 toast(result.message)
             
             })
@@ -42,7 +44,8 @@ const Shorten = () => {
 
     return (
         <>
-        <div className='mx-auto max-w-lg bg-purple-100 my-32 p-8 rounded-lg flex flex-col gap-4'>
+        <div className='flex justify-center items-center h-screen bg-purple-100'>
+            <div className='mx-auto max-w-lg bg-purple-200 my-32 p-8 rounded-lg flex flex-col gap-4'>
             <h1 className='font-bold text-2xl text-purple-500'>Generate your short URLs</h1>
             <div className='flex flex-col gap-2'>
                 <input type="text"
@@ -56,11 +59,12 @@ const Shorten = () => {
                     className='px-4 py-2 focus:outline-purple-600 rounded-md'
                     placeholder='Enter your preferred short URL text'
                     onChange={e => { setshorturl(e.target.value) }} />
-                <button onClick={generate} className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 my-3 font-bold text-white'>Generate</button>
+                <button onClick={generate} className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 my-3 font-bold text-white'>{loading ? "Generating..." : "Generate"}</button>
             </div>
 
             {generated && <> <span className='font-bold text-lg'>Your Link </span><code><Link target="_blank" href={generated}>{generated}</Link> 
                 </code></>}
+        </div>
         </div>
         <ToastContainer position="bottom-right" />
         </>
